860c1970b8044968c8c982a3b0f451086dc85a6b
Rozen Snowflakes <draftencode@gmail.com>
2017-09-01 11:03:22 -0400

    00

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo("<style>
+body{
+  background: #466368;
+  background: -webkit-linear-gradient(#648880, #293f50);
+  background:    -moz-linear-gradient(#648880, #293f50);
+  background:         linear-gradient(#648880, #293f50);
+}
+</style>");
+echo("<audio style='Position:Fixed;Left:0%;Right:0%;Bottom:0%;Width:100vw;Height:Auto;Display:Inline-Block;' src='[zero]' controls loop></audio>");
+echo("<title>::</title>");
+echo("<code>Operation Internet Freedom :: Project Fishnet.</code>");
+echo("<hr/>");
+echo("<code>If you can see this Webpage, It only means you have a Free Internet.</code>");
+echo("<hr/>");
+echo("<code>Developed by Night Core Co Ltd & Art Mydeas Co Ltd.</code>");
+?>
